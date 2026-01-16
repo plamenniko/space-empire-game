0420e1d1992272735215a85f0871192890b44acd
plamenniko <6td5w4498j@privaterelay.appleid.com>
2026-01-17 00:58:55 +0100

    Update renderer.js

--- js/renderer.js
+++ js/renderer.js
@@ -1,15 +1,28 @@
 console.log("renderer.js loaded");
 
-window.render = function () {
-  const ctx = window.ctx;
+window.render = function (ctx) {
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
-  ctx.fillStyle = "black";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.save();
+  ctx.translate(ctx.canvas.width / 2, ctx.canvas.height / 2);
+  ctx.scale(camera.zoom, camera.zoom);
+  ctx.translate(-camera.x, -camera.y);
 
+  // stars
   ctx.fillStyle = "white";
-  for (const fleet of window.fleets) {
+  for (const s of galaxy.stars) {
     ctx.beginPath();
-    ctx.arc(fleet.x, fleet.y, 4, 0, Math.PI * 2);
+    ctx.arc(s.x, s.y, s.r, 0, Math.PI * 2);
     ctx.fill();
   }
+
+  // systems
+  ctx.fillStyle = "yellow";
+  for (const sys of systems) {
+    ctx.beginPath();
+    ctx.arc(sys.x, sys.y, sys.r, 0, Math.PI * 2);
+    ctx.fill();
+  }
+
+  ctx.restore();
 };
