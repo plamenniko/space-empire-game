a4de2a49747365ded0678927d983793231a87681
plamenniko <6td5w4498j@privaterelay.appleid.com>
2026-01-17 00:03:32 +0100

    Initialize canvas and animation loop in core.js

--- js/core.js
+++ js/core.js
@@ -1,2 +1,25 @@
-// core.js
-console.log("core loaded");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+function resize() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+window.addEventListener("resize", resize);
+resize();
+
+let lastTime = performance.now();
+
+function loop(time) {
+  const dt = (time - lastTime) / 1000;
+  lastTime = time;
+
+  updateCamera(dt);
+  updateFleets(dt);
+
+  drawScene();
+
+  requestAnimationFrame(loop);
+}
+
+requestAnimationFrame(loop);
