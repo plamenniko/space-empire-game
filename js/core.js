3b7faf3dddf2ecb739137bf11729d8b690562549
plamenniko <6td5w4498j@privaterelay.appleid.com>
2026-01-17 00:51:00 +0100

    Update core.js

--- js/core.js
+++ js/core.js
@@ -1,31 +1,15 @@
-// core.js
 console.log("core.js loaded");
 
 const canvas = document.getElementById("game");
-const ctx = canvas.getContext("2d");
+window.canvas = canvas;
 
-function resize() {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-}
-window.addEventListener("resize", resize);
-resize();
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+window.ctx = canvas.getContext("2d");
 
 function loop() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  // draw fleet dot
-  ctx.fillStyle = "white";
-  ctx.beginPath();
-  ctx.arc(
-    canvas.width / 2,
-    canvas.height / 2,
-    4,
-    0,
-    Math.PI * 2
-  );
-  ctx.fill();
-
+  render();
   requestAnimationFrame(loop);
 }
 
