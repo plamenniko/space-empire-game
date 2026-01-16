4f0983a28b06d290909fe292e5d7f842f8c70511
plamenniko <6td5w4498j@privaterelay.appleid.com>
2026-01-17 00:55:39 +0100

    Update galaxy.js

--- js/galaxy.js
+++ js/galaxy.js
@@ -1,1 +1,14 @@
 console.log("galaxy.js loaded");
+
+window.galaxy = {
+  stars: []
+};
+
+// create background stars
+for (let i = 0; i < 200; i++) {
+  galaxy.stars.push({
+    x: Math.random() * 4000 - 2000,
+    y: Math.random() * 4000 - 2000,
+    r: Math.random() * 1.5 + 0.5
+  });
+}
