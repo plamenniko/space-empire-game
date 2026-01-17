5ee0d8f0d84f91baaf811226ebf1cede189c2f72
plamenniko <6td5w4498j@privaterelay.appleid.com>
2026-01-17 01:04:03 +0100

    Update renderer.js

--- js/renderer.js
+++ js/renderer.js
@@ -5,23 +5,20 @@ window.render = function (ctx) {
 
   ctx.save();
   ctx.translate(ctx.canvas.width / 2, ctx.canvas.height / 2);
-  ctx.scale(camera.zoom, camera.zoom);
-  ctx.translate(-camera.x, -camera.y);
 
-  // stars
-  ctx.fillStyle = "white";
-  for (const s of galaxy.stars) {
-    ctx.beginPath();
-    ctx.arc(s.x, s.y, s.r, 0, Math.PI * 2);
-    ctx.fill();
-  }
+  for (const layer of galaxy.layers) {
+    ctx.save();
+    ctx.scale(camera.zoom * layer.depth, camera.zoom * layer.depth);
+    ctx.translate(-camera.x, -camera.y);
 
-  // systems
-  ctx.fillStyle = "yellow";
-  for (const sys of systems) {
-    ctx.beginPath();
-    ctx.arc(sys.x, sys.y, sys.r, 0, Math.PI * 2);
-    ctx.fill();
+    ctx.fillStyle = "white";
+    for (const s of layer.stars) {
+      ctx.beginPath();
+      ctx.arc(s.x, s.y, s.r, 0, Math.PI * 2);
+      ctx.fill();
+    }
+
+    ctx.restore();
   }
 
   ctx.restore();
