8b3ff4d77c47ffce76c06f4d8a581e39dc047212
plamenniko <6td5w4498j@privaterelay.appleid.com>
2026-01-17 02:05:58 +0100

    Update core.js

--- js/core.js
+++ js/core.js
@@ -1,1 +1,10 @@
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+
+function resize() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+window.addEventListener("resize", resize);
+resize();
 
