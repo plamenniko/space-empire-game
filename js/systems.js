8ab945333f868246264061eb54805b6e1e65c358
plamenniko <6td5w4498j@privaterelay.appleid.com>
2026-01-17 01:11:03 +0100

    Update systems.js

--- js/systems.js
+++ js/systems.js
@@ -1,5 +1,15 @@
 console.log("systems.js loaded");
 
-window.systems = [
-  { x: 0, y: 0, r: 12 }
+const systems = [
+  { x: 0, y: 0, r: 40 },
+  { x: 600, y: -300, r: 30 }
 ];
+
+function drawSystems(ctx) {
+  ctx.fillStyle = "yellow";
+  for (const s of systems) {
+    ctx.beginPath();
+    ctx.arc(s.x, s.y, s.r, 0, Math.PI * 2);
+    ctx.fill();
+  }
+}
