29628b9efb32c3158255a40fd83670b138d8b712
plamenniko <6td5w4498j@privaterelay.appleid.com>
2026-01-17 02:06:54 +0100

    Update renderer.js

--- js/renderer.js
+++ js/renderer.js
@@ -1,1 +1,10 @@
+function beginRender() {
+  ctx.setTransform(1, 0, 0, 1, 0, 0);
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  ctx.translate(canvas.width / 2, canvas.height / 2);
+  ctx.scale(camera.zoom, camera.zoom);
+  ctx.translate(-camera.x, -camera.y);
+}
 
